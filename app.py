9e59903e64c78f3ba88ff98eeb9dacb981e7c73c
Dương Ngọc Hải <dngochai0511@gmail.com>
2023-11-16 09:42:48 +0700

    get netflix film by show_id

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+import json
 from flask import Flask, jsonify,request
 
 from flask_cors import CORS
@@ -33,8 +33,35 @@ def get_netflix_data():
     else:
         # Nếu không có tham số 'page', trả về thông báo
         return 'full data will update soon', 200, {'Content-Type': 'text/plain'}
+    
+@app.route('/api/netflix/<show_id>', methods=['GET'])
+def get_show_by_id(show_id):
+    with open('./data/dataPage1.json', 'r') as file:
+        data = json.load(file)
+    # Tìm kiếm show với show_id tương ứng
+    show = next((item for item in data['results'] if item['show_id'] == show_id), None)
 
-
+    if show:
+        # Chuyển định dạng dữ liệu để trả về
+        result = {
+           'show_id': show['show_id'],
+            'type': show['type'],
+            'title': show['title'],
+            'director': show['director'],
+            'cast': show['cast'],
+            'country': show['country'],
+            'date_added': show['date_added'],
+            'release_year': show['release_year'],
+            'rating': show['rating'],
+            'duration': show['duration'],
+            'listed_in': show['listed_in'],
+            'description': show['description'],
+            'backdrop_path': show['backdrop_path'],
+            'poster_path': show['poster_path']
+        }
+        return jsonify(result)
+    else:
+        return jsonify({'error': 'Show not found'}), 404
 
 @app.route("/api/<id>/recommendations")
 def get_recommend(id):
@@ -43,8 +70,6 @@ def get_recommend(id):
         return jsonify(recommendation.convert_to_desired_format(data))
     except Exception as e:
         return jsonify({"error": str(e)})
-
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=9000)
